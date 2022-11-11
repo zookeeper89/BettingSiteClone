88f0aa4109964c818ca0579e1b15fc813289ac1b
daprior <daniel_wow1@live.dk>
2022-11-11 19:21:35 +0100

    asd

--- components/Hero/TopHero.js
+++ components/Hero/TopHero.js
@@ -7,7 +7,7 @@ export default function TopHero(props) {
         <div className="">
           <div className="mx-auto max-w-7xl lg:px-8 ">
             <div className="">
-              <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:flex lg:items-center">
+              <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0  lg:items-center">
                 <div className="lg:py-24">
                   <div className=" rounded-xl   ">
                     <h1 className="mt-4 pb-3 text-4xl tracking-tight font-bold text-white sm:mt-5 sm:text-6xl sm:tracking-tight lg:mt-6 xl:text-6xl xl:tracking-tight">
@@ -26,7 +26,7 @@ export default function TopHero(props) {
                       </div>
                     </h1>
                     <p className="mt-3 text-center text-white font-semibold text-base text- sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                     Få 20% rabat med koden Sommer
+                      Få 20% rabat med koden Sommer
                     </p>
                   </div>
                 </div>
